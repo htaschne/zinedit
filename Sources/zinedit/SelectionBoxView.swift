17a6565a909407bd9e6c6c9194ba96802faa51cd
Agatha Schneider <agatha.schneider@outlook.com>
2025-11-27 15:04:42 -0300

    change color of dash border

--- Sources/zinedit/SelectionBoxView.swift
+++ Sources/zinedit/SelectionBoxView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct SelectionBox: View {
-    // TODO: this should be scaled, positioned and rotated along with it's content
     var body: some View {
         RoundedRectangle(cornerRadius: 8)
             .stroke(style: StrokeStyle(lineWidth: 1, dash: [6]))
+            .foregroundStyle(Color("BrandZinerPrimary100"))
             .padding(-6)
     }
 }
