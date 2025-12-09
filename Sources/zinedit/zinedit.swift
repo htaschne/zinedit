b27e694ab150bb8bb2cccfcc6d7487bd93e22719
Endrew Soares <endrewmss@hotmail.com>
2025-12-09 16:24:38 -0300

    feat: better image quality

--- Sources/zinedit/zinedit.swift
+++ Sources/zinedit/zinedit.swift
@@ -334,7 +334,6 @@ public struct EditorCanvasView: View {
                     } label: {
                         Image(systemName: "chevron.left")
                     }
-                    .disabled(!model.canUndo)
                     .accessibilityIdentifier("undoTopButton")
                 }
                 
